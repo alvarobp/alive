9d0ae7cfc6f8a50c83c5253404507186fdf8b29b
Alvaro Bautista <alvarobp@gmail.com>
2011-11-12 17:31:07 +0100

    Rule #4: Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction

--- lib/alive/world.rb
+++ lib/alive/world.rb
@@ -4,16 +4,35 @@ module Alive
 
     def initialize
       @cells = []
+      @dead_cells = []
     end
 
     def tick!
+      @next_alive_cells = []
+      @next_dead_cells = []
+      
       @cells.each do |cell|
-        @cells -= [cell] if cell.neighbours.count < 2 || cell.neighbours.count > 3
+        if cell.neighbours.count < 2 || cell.neighbours.count > 3
+          @next_dead_cells << cell
+        else
+          @next_alive_cells << cell
+        end
       end
+      
+      @dead_cells.each do |cell|
+        if cell.neighbours.count == 3
+          @dead_cells -= [cell]
+          @next_alive_cells << cell
+        end
+      end
+      
+      @dead_cells += @next_dead_cells
+      @cells = @next_alive_cells
     end
 
     def clear
       @cells = []
+      @dead_cells = []
     end
   end
 end
--- spec/alive_spec.rb
+++ spec/alive_spec.rb
@@ -33,8 +33,36 @@ describe "Alive" do
     cell.spawn_at(0,1)
     cell.spawn_at(1,1)
     cell.spawn_at(1,-1)
+    cell.neighbours.count.should == 4
 
     world.tick!
     cell.dead?.should be_true
+  end
+
+  it "Rule #4: Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction" do
+    cell = Alive::Cell.new(world)
+    new_cell = cell.spawn_at(1,0)
+    world.tick!
+    cell.dead?.should be_true
+
+    cell.spawn_at(0,1)
+    cell.spawn_at(-1,0)
+    cell.spawn_at(-1,1)
+    cell.neighbours.count.should == 3
+    world.tick!
+    cell.alive?.should be_true
+
+    world.clear
+    cell = Alive::Cell.new(world)
+    new_cell = cell.spawn_at(1,0)
+    world.tick!
+
+    cell.spawn_at(0,1)
+    cell.spawn_at(-1,0)
+    cell.spawn_at(-1,1)
+    cell.spawn_at(1,1)
+    cell.neighbours.count.should == 4
+    world.tick!
+    cell.dead?.should be_true # stays dead
   end
 end
